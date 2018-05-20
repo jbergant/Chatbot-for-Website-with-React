feba88f6a6e250fe42a39e32cf505359a13e76bb
Jana Bergant <jana.bergant@gmail.com>
2018-05-20 19:22:17 +0200

    S5 - 010 - Simple Message component

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-
 import axios from "axios/index";
+
+import Message from './Message';
 
 class Chatbot extends Component {
 
@@ -47,12 +48,26 @@ class Chatbot extends Component {
             this.setState({ messages: [...this.state.messages, says]});
         }
     };
+    
+
+    renderMessages(returnedMessages) {
+        if (returnedMessages) {
+            return returnedMessages.map((message, i) => {
+                    return <Message key={i} speaks={message.speaks} text={message.msg.text.text}/>;
+                }
+            )
+        } else {
+            return null;
+        }
+    }
+
 
     render() {
         return (
             <div style={{height: 400, width: 400, float: 'right'}}>
                 <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
                     <h2>Chatbot</h2>
+                    {this.renderMessages(this.state.messages)}
                     <input type="text"/>
                 </div>
             </div>
