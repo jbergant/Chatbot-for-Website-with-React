65123e306a7ef89178784dca6f1cf5551c2f5603
Jana Bergant <jana.bergant@gmail.com>
2018-05-20 17:20:51 +0200

    S4 - 007 - Event query to DialogFlow

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,5 +1,6 @@
 'use strict';
 const dialogflow = require('dialogflow');
+const structjson = require('./structjson.js');
 const config = require('../config/keys');
 
 const projectId = config.googleProjectID;
@@ -42,7 +43,24 @@ module.exports = {
 
     },
 
+    eventQuery: async function(event, parameters = {}) {
+        let self = module.exports;
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                event: {
+                    name: event,
+                    parameters: structjson.jsonToStructProto(parameters), //Dialogflow's v2 API uses gRPC. You'll need a jsonToStructProto method to convert your JavaScript object to a proto struct.
+                    languageCode: languageCode,
+                },
+            }
+        };
 
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses);
+        return responses;
+
+    },
 
 
     handleAction: function(responses){
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -15,7 +15,8 @@ module.exports = app => {
 
     });
 
-    app.post('/api/df_event_query', (req, res) => {
-        res.send({'do': 'event query'})
+    app.post('/api/df_event_query', async (req, res) => {
+        let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
+        res.send(responses[0].queryResult);
     });
 }
