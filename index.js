42fed1979651ce838e9e8ad8216d4377bbbf5f34
Jana Bergant <jana.bergant@gmail.com>
2018-05-25 10:27:10 +0200

    S9 - 002 - FrontEnd routing on Server

--- index.js
+++ index.js
@@ -17,5 +17,17 @@ app.use(bodyParser.json());
 require('./routes/dialogFlowRoutes')(app);
 require('./routes/fulfillmentRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    // js and css files
+    app.use(express.static('client/build'));
+
+    // index.html for all page routes
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
