e247548e30f8b116c8dfea217f4683c45b03ac81
Jana Bergant <jana.bergant@gmail.com>
2018-09-26 11:07:50 +0200

    S4 - 008 - Config local & server

---
+++ config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleProjectID: process.env.GOOGLE_PROJECT_ID,
+    dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
+    dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_LANGUGAGE_CODE,
+    googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+    googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY,
+};
