a05993fc3d0df9d9941053cf41631344cc0fefc5
Jana Bergant <jana.bergant@gmail.com>
2018-11-14 11:50:09 +0100

    S4-004-DialogFlow-module-Authentication-setup-part-2

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleProjectID: 'reactpageagent',
+    dialogFlowSessionID: 'react-bot-session',
+    dialogFlowSessionLanguageCode: 'en-US'
+}
--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -1,3 +1,11 @@
+const dialogflow = require('dialogflow');
+const config = require('../config/keys');
+
+const sessionClient = new dialogflow.SessionsClient();
+
+const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
+
+
 module.exports = app => {
 
     app.get('/', (req, res) => {
@@ -5,6 +13,32 @@ module.exports = app => {
     });
 
     app.post('/api/df_text_query', (req, res) => {
+
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                text: {
+                    text: req.body.text,
+                    languageCode: config.dialogFlowSessionLanguageCode
+                }
+            }
+        };
+        sessionClient
+            .detectIntent(request)
+            .then(responses => {
+                console.log('Detected intent');
+                const result = responses[0].queryResult;
+                console.log(`  Query: ${result.queryText}`);
+                console.log(`  Response: ${result.fulfillmentText}`);
+                if (result.intent) {
+                    console.log(`  Intent: ${result.intent.displayName}`);
+                } else {
+                    console.log(`  No intent matched.`);
+                }
+            })
+            .catch(err => {
+                console.error('ERROR:', err);
+            });
         res.send({'do': 'text query'})
     });
 
