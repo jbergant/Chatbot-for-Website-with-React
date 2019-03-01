c2cce619babbb21607cf5b6750bfb1d7d5996b58
Jana Bergant <jana.bergant@gmail.com>
2019-03-01 12:59:56 +0100

    S5-008-Connect-Server-and-Client

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class Chatbot extends Component {
+    
+    constructor(props) {
+        super(props);
 
-const Chatbot = () => (
-    <h2>Chatbot will be here</h2>
-)
+        this.state = {
+            messages: []
+        };
+    }
+
+    render() {
+        return (
+            <div style={{height: 400, width: 400, float: 'right'}}>
+                <div id="chatbot" style={{height: '100%', width: '100%', overflow: 'auto'}}>
+                    <h2>Chatbot</h2>
+                    <input type="text"/>
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Chatbot;
