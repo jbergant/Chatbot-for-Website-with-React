f40d3adac88d94a32ca991952ac32c6301e08a1b
Jana Bergant <jana.bergant@gmail.com>
2019-03-01 11:43:37 +0100

    S5 - 005 – Components for the page - part 1

---
+++ client/src/components/pages/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div><h1>Selling you great stuff!</h1>
+        with the help of the chatbot
+        </div>
+    )
+}
+
+export default Landing;
