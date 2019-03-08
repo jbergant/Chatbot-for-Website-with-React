781825a98b0eb17e5a76ea9c1561a3f698bf32c9
Jana Bergant <jana.bergant@gmail.com>
2019-03-08 15:36:45 +0100

    S6 - 009 – Quick-replies-components-implementation

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -6,6 +6,7 @@ import { v4 as uuid } from 'uuid';
 
 import Message from './Message';
 import Card from './Card';
+import QuickReplies from './QuickReplies';
 
 const cookies = new Cookies();
 
@@ -103,6 +104,17 @@ class Chatbot extends Component {
                     </div>
                 </div>
             </div>
+        } else if (message.msg &&
+            message.msg.payload &&
+            message.msg.payload.fields &&
+            message.msg.payload.fields.quick_replies
+        ) {
+            return <QuickReplies
+                text={message.msg.payload.fields.text ? message.msg.payload.fields.text : null}
+                key={i}
+                replyClick={this._handleQuickReplyPayload}
+                speaks={message.speaks}
+                payload={message.msg.payload.fields.quick_replies.listValue.values}/>;
         }
     }
 
