c32397c7280ef8507bafc1006c2c2220eae94445
Jana Bergant <jana.bergant@gmail.com>
2019-03-11 20:12:08 +0100

    S9 - 005 - with service worker

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
